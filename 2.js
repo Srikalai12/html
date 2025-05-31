89eaeeddb1412e7f210bd711da728f4b93b61219
Srikalai12 <srikalaivani393@gmail.com>
2025-05-31 12:20:08 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,11 @@
+function printall(){
+   
+        var numberthree=document.getElementById("name").value 
+      var numberfour=document.getElementById("value").value 
+      var numberfive=document.getElementById("number").value 
+      var numbersix=document.getElementById("values").value 
+    document.getElementById("valuesare").innerHTML=numberthree
+    document.getElementById("num").innerHTML=numberfour
+
+    document.getElementById("numbers").innerHTML=numberfour*numberfive
+}
